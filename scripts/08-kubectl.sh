ca704754e93ade0ada5ce9e44364203d0c583f41
Hasiotis Nikos <hasiotis@gmail.com>
2018-07-31 20:12:50 +0300

    More k8s tooling

--- scripts/08-kubectl.sh
+++ scripts/08-kubectl.sh
@@ -4,3 +4,11 @@ cd ~/tmp
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+chmod +x ./kubectx
+sudo mv ./kubectx /usr/local/bin/kubectx
+
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+chmod +x ./kubens
+sudo mv ./kubens /usr/local/bin/kubens
