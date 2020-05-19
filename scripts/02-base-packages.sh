193541d40a3443b3079fa15187323da53a4e9f5a
Nikos Chasiotis <hasiotis@gmail.com>
2020-05-19 10:59:34 +0300

    Updates

--- scripts/02-base-packages.sh
+++ scripts/02-base-packages.sh
@@ -3,5 +3,5 @@ echo "- Install additional packages"
 sudo apt-get install -y \
     vim fonts-inconsolata git mc fonts-font-awesome ssh \
     unison lftp keychain vim-pathogen python3-pip \
-    python-pip htop net-tools curl
+    python-pip htop net-tools curl httpie netcat
 
