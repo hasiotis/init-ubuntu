1492a96295555e346931f74334894932f916c8b7
Nikos Chasiotis <hasiotis@gmail.com>
2020-05-19 11:06:43 +0300

    Updates

--- initUbuntu.sh
+++ initUbuntu.sh
@@ -7,5 +7,7 @@ done
 
 if [ ! -e ~/.bashrc.ORIG ] ; then
     cp ~/.bashrc ~/.bashrc.ORIG && cp files/bashrc ~/.bashrc
+    cp files/bashrc_local ~/.bashrc_local
+    cp files/bash_aliases ~/.bashrc_aliases
     cp -r files/powerline ~/.config
 fi
