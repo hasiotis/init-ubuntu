edf9c9ff13414e88be3f052fc619418a494c0179
Nikos Chasiotis <hasiotis@gmail.com>
2020-05-19 11:29:46 +0300

    Some tmux stuff

--- scripts/02-base-packages.sh
+++ scripts/02-base-packages.sh
@@ -3,5 +3,6 @@ echo "- Install additional packages"
 sudo apt-get install -y \
     vim fonts-inconsolata git mc fonts-font-awesome ssh \
     unison lftp keychain vim-pathogen python3-pip \
-    jq htop net-tools curl httpie netcat
+    jq htop net-tools curl httpie netcat \
+    tmux-plugin-manager tmux
 
