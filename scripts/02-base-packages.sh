0224168a16406d52c30b8d95fc6851ac5396baa0
Nikos Chasiotis <hasiotis@gmail.com>
2021-10-19 17:26:15 +0300

    More packages

--- scripts/02-base-packages.sh
+++ scripts/02-base-packages.sh
@@ -3,6 +3,9 @@ echo "- Install additional packages"
 sudo apt-get install -y \
     vim fonts-inconsolata git mc fonts-font-awesome ssh \
     unison lftp keychain vim-pathogen python3-pip \
-    jq htop net-tools curl httpie netcat \
-    tmux-plugin-manager tmux
+    jq htop net-tools curl httpie netcat bat \
+    tmux-plugin-manager tmux direnv python3-boto3 \
+    pipx tig virtualbox virtualbox-dkms
 
+pip3 install --user ansible
+pix install zbuilder
