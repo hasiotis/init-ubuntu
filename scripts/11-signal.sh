4c07d0020807931b318c273c5f407b8db6264147
Nikos Chasiotis <hasiotis@gmail.com>
2021-10-22 15:51:01 +0300

    Install signal app

---
+++ scripts/11-signal.sh
@@ -0,0 +1,9 @@
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+rm signal-desktop-keyring.gpg
+
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt update
+sudo apt-get install -y signal-desktop
