37c83df00fe1adf61474e0d2cc73330167afb5cb
Nikos Chasiotis <hasiotis@gmail.com>
2022-06-02 10:12:09 +0300

    Set gnome terminal

--- scripts/03-gnome-terminal.sh
+++ scripts/03-gnome-terminal.sh
@@ -5,3 +5,9 @@ git clone https://github.com/arcticicestudio/nord-gnome-terminal.git
 cd nord-gnome-terminal/src
 ./nord.sh
 rm -rf /tmpnord-gnome-terminal
+
+GNOME_TERMINAL_PROFILE=`dconf dump /org/gnome/terminal/legacy/profiles:/ | awk '/\[:/||/visible-name=/' | grep -B 1 Nord | grep -v Nord | tr -d \]\[:`
+gsettings set org.gnome.Terminal.ProfilesList default ${GNOME_TERMINAL_PROFILE}
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ font 'Source Code Pro 11'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ use-system-font false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ audible-bell false
