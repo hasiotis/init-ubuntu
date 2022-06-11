f146e1c80ba4ccb9c6948af0afb21c04ac85f1a0
Nikos Chasiotis <hasiotis@gmail.com>
2022-06-11 15:55:08 +0300

    Kubernetes update

--- scripts/09-kubernetes.sh
+++ scripts/09-kubernetes.sh
@@ -8,13 +8,13 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 cd -
 
 (
-  cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.4.2/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update &&
-  cd -
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
 )
 
 kubectl krew install ctx
