0c75059b5df963bae6cb5f4af53eeafdd9c237e5
Nikos Chasiotis <hasiotis@gmail.com>
2022-06-11 15:54:19 +0300

    Install google chrome

---
+++ scripts/11-google-chrome.sh
@@ -0,0 +1,5 @@
+echo "- Install chrome"
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
