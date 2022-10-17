32b31b7f15763422c1a6a65d01528d410a92de36
Nikos Chasiotis <hasiotis@gmail.com>
2022-10-17 10:19:14 +0300

    Moooare

--- scripts/15-python.sh
+++ scripts/15-python.sh
@@ -2,3 +2,9 @@ echo "Install python stuff"
 
 curl -sSL https://install.python-poetry.org | python3 -
 
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa --yes
+
+sudo apt install -y python3.7 python3.7-dev python3.7-venv
+sudo apt install -y python3.8 python3.8-dev python3.8-venv
+sudo apt install -y python3.9 python3.9-dev python3.9-venv
---
+++ scripts/16-tailscale.sh
@@ -0,0 +1,7 @@
+echo "Install tailscale"
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt-get update
+sudo apt-get install tailscale
