223f2c99c85949df89f2c42cd6557af0425262cb
Nikos Chasiotis <hasiotis@gmail.com>
2024-04-18 13:16:31 +0300

    netcat-openbsd

--- scripts/02-base-packages.sh
+++ scripts/02-base-packages.sh
@@ -3,7 +3,7 @@ echo "- Install additional packages"
 sudo apt-get install -y \
     vim fonts-inconsolata git mc fonts-font-awesome ssh \
     unison lftp keychain vim-pathogen python3-pip \
-    jq htop net-tools curl httpie netcat bat \
+    jq htop net-tools curl httpie netcat-openbsd bat \
     tmux-plugin-manager tmux direnv python3-boto3 \
     pipx tig virtualbox virtualbox-dkms unison cmake
 
