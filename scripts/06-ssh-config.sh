d6e3f296f2bdc04669fcadd44fd1e47e3da49e92
Nikos Chasiotis <hasiotis@gmail.com>
2024-04-26 17:45:14 +0300

    Get ssh pub key from github

--- scripts/06-ssh-config.sh
+++ scripts/06-ssh-config.sh
@@ -1,3 +1,3 @@
 echo "- Configure ssh"
 
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVrU/gQjDq0xv0sCHJoB/Vqgs5xqltdzpgzLPW9JNLa hasiotis@gmail.com" > ~/.ssh/authorized_keys
+curl https://github.com/hasiotis.keys > ~/.ssh/authorized_keys
