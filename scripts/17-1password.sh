2f1309fdc27fedf1090b667612980255ecb862b1
Nikos Chasiotis <hasiotis@gmail.com>
2024-05-09 10:16:43 +0300

    1password fix

--- scripts/17-1password.sh
+++ scripts/17-1password.sh
@@ -6,4 +6,4 @@ sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-apt update && apt-get install -y 1password-cli 1password
+sudo apt-get update && sudo apt-get install -y 1password-cli 1password
