cead054cd5719d8fe8a9875b4a9c365300292065
Nikos Chasiotis <hasiotis@gmail.com>
2024-07-02 14:44:21 +0300

    Add aws-vault install

--- scripts/10-cloud.sh
+++ scripts/10-cloud.sh
@@ -3,3 +3,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf ./aws awscliv2.zip
+
+echo "- Install aws-vault"
+curl -Lo ./aws-vault https://github.com/99designs/aws-vault/releases/download/v7.2.0/aws-vault-linux-amd64
+chmod +x ./aws-vault
+mv ./aws-vault ~/.local/bin/aws-vault
